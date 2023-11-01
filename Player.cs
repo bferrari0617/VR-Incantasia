b906b744ff3863646a3fc4e4b2e610e15cac2339
bferrari0617 <baferrar@syr.edu>
2023-11-01 15:48:26 -0400

    Added more basic character controller code

--- Player.cs
+++ Player.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public CharacterController;
 public class Player : MonoBehaviour
 {
+    public CharacterController character = null;
+    public float speed = 6.0f;
+    public Vector3 velocity;
     // Start is called before the first frame update
     void Start()
     {
-        
+        character = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
