52512a44f84760b70ee77bf6a657b2bdf5b2338a
bferrari0617 <baferrar@syr.edu>
2023-11-01 15:29:58 -0400

    added character controller

--- Player.cs
+++ Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public CharacterController;
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
