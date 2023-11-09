53d6466169b21265795a150ed6e667dcdce142f3
Daniel <danielchm6@gmail.com>
2023-11-09 12:54:13 -0500

    +ARROW

--- IncantasiaProject/Assets/Scripts/Arrow.cs
+++ IncantasiaProject/Assets/Scripts/Arrow.cs
@@ -2,5 +2,15 @@
 
 public class Arrow : MonoBehaviour
 {
+    public float force = 2000.0f;
+    public Transform tip = null;
+    public Rigidbody rbArrow = null;
+    public bool hitTarget = true;
+
+    public Vector3 last_pos = Vector3.zero;
+
+    public void Start(){
+        
+    }
    
 }
