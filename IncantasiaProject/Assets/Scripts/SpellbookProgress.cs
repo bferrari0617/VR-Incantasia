72778c2f157251dfe0154685dca5c59ba68e2b53
Daniel <danielchm6@gmail.com>
2023-12-05 15:30:54 -0500

    +dec5

--- IncantasiaProject/Assets/Scripts/SpellbookProgress.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SpellbookProgress", menuName = "ScriptableObjects/SpellbookProgress", order = 1)]
-public class SpellbookProgress : ScriptableObject
-{
-    public List<Texture> spellTextures;
-}
